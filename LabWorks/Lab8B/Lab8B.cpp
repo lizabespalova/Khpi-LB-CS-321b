e1536c8788f7372abe5c74aef22ddddb28235544
lizabespalova <bespalovalizv@gmail.com>
2021-11-25 14:13:22 +0200

    Update Lab8B.cpp

--- LabWorks/Lab8B/Lab8B.cpp
+++ LabWorks/Lab8B/Lab8B.cpp
@@ -1,13 +1,93 @@
 ﻿// Lab8B.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
 //
 
+
 #include <iostream>
+//#include <algorithm>
+#include <iomanip>
+#include<windows.h>
+//#include <string>
+
+using namespace std;
+
+
+
 
 int main()
-{
-    std::cout << "Hello World!\n";
-}
 
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    int arr_size;
+    cout << "Введите количество элементов массива: ";
+    cin >> arr_size;
+   
+    int k; 
+    int i; 
+    int j; 
+    cout << "Ваш массив:" << endl;
+    for (i = 0; i < arr_size; i++) {
+        arr[i] = rand() % 10 + 1;
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    char num; 
+    char udal = '-';
+    char dobav = '+';
+    while (true) {
+        cout << "Вы хотите удалить или добавить элементы? <-/+> ";
+        cin >> num;
+        if (num == dobav) {
+            int numstr; 
+            cout << "Сколько элементов вы хотите добавить? ";
+            cin >> k
+            cout << "Выберите позицию, с которой вы хотите добавть данное количество элементов: " << k << endl;
+            cin >> numstr;
+            
+            for (i = 0; i < numstr; i++) {
+                add_new_arr[i] = arr[i];
+            }
+            
+            cout << "Новые элементы: ";
+            for (j = 0; j < k; j++) {
+                add_elements[j] = rand() % 10 + 1;
+                cout << add_elements[j] << " ";
+            }
+            cout << endl;
+            for (j = 0, i = numstr; i < arr_size + k; i++, j++) {
+                add_new_arr[i] = add_elements[j];
+            }
+            for (j = numstr + k, i = numstr; j < arr_size + k; j++, i++) {
+                add_new_arr[i + k] = arr[i];
+            }
+            cout << "Ваш новый массив:" << endl;
+            for (i = 0; i < arr_size + k; i++) {
+                cout << add_new_arr[i] << " ";
+            }
+            cout << endl;
+        }
+        else if (num == udal) {
+            cout << "Сколько элементов вы хотите удалить? ";
+            cin >> k;
+            cout << "Выберите позицию, с которой вы хотите удалить данное количество элементов: " << k << endl;
+            cin >> i;
+            if (arr_size == k and i == 0) {
+                cout << "Массив удалён!" << endl;
+            }
+            else {
+                for (; i < arr_size - k; i++) {
+                    arr[i] = arr[i + k];
+                }
+                cout << "Ваш новый массив:" << endl;
+                for (i = 0; i < arr_size - k; i++) {
+                    cout << arr[i] << " ";
+                }
+                cout << endl;
+            }
+        }
+    }
+    return 0;
+}
 // Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
 // Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
 
