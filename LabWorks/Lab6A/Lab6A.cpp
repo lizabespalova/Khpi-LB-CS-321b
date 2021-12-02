c5bc88ad85e8606c538ca18197880bf141b86c56
lizabespalova <bespalovalizv@gmail.com>
2021-12-02 12:25:51 +0200

    6А

--- LabWorks/Lab6A/Lab6A.cpp
+++ LabWorks/Lab6A/Lab6A.cpp
@@ -2,10 +2,30 @@
 //
 
 #include <iostream>
+#include <windows.h>
+#include <ctime>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+using namespace std;
+
+int main() {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	srand(time(NULL));
+	int comp_num = rand() % 100;
+	int a = 0;
+	int popytky = 0;
+	while (comp_num != a) {
+		popytky++;
+		cout << "Введите число от 1 до 100\n";
+		cin >> a;
+		if (a < comp_num)
+			cout << " Число компьютера больше вашего\n";
+		if (a > comp_num)
+			cout << "Число компьютера меньше вашего\n ";
+	}
+	if (comp_num = a)
+		cout << "Ура! вы угадали\n" << popytky << "-У вас было столько попыток\n";
+	return 0;
 }
 
 // Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
