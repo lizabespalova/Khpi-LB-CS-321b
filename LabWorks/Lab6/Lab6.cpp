963efd9e00dd55db96744337297b08df2baef19b
lizabespalova <bespalovalizv@gmail.com>
2021-12-02 12:23:48 +0200

    докинула 6

--- LabWorks/Lab6/Lab6.cpp
+++ LabWorks/Lab6/Lab6.cpp
@@ -2,12 +2,39 @@
 //
 
 #include <iostream>
+#include <math.h>
+#include <cmath>
+#include <stdio.h>
+#include <windows.h>
+#pragma warning (disable : 4996);
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
-}
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	long n;
+	double dbln;
+	double term;
+	double sum = 0;
+	short k1 = 1;
+	const double eps = 0.000001;
+	for (n = 0; ; k1 = -k1, n++)
+	{
+		dbln = n;
+		term = k1 * (1 - (2 * dbln - 1) / (2 * (dbln + 1)));
+		if (fabs(term) >= eps)
+			sum += term;
+		else break;
+		if (n == 9)
+			printf("Сумма 10 членов ряда =%10.7lf\n", sum);
+	}
+	printf("Полная сумма ряда = %10.7lf\n", sum);
+	return 0;
 
+
+
+}
 // Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
 // Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
 
