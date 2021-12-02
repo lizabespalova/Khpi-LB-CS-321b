f17ba3e872182e02e5725d9826966d0c9bb5a608
lizabespalova <bespalovalizv@gmail.com>
2021-12-02 12:34:44 +0200

    8А

--- LabWorks/Lab8A/Lab8A.cpp
+++ LabWorks/Lab8A/Lab8A.cpp
@@ -1,11 +1,73 @@
 ﻿// Lab8A.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
 //
-
 #include <iostream>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+#include <math.h>
+
+#include <iomanip>
+
+#include <ctime>
+
+#include <string>
+
+using namespace std;
+
+
+
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    int Ar[6];
+    int Mass[6];
+    int i = 0;
+    int b = 0;
+    int all_places = 0;
+    int smoking_place = 0;
+    string a;
+    int no_smoking_place = 0;
+
+
+    cout << "Вас приветствует компания UkraineAirLines" << endl;
+
+    cout << "Для бронирования билетов сообщите, пожалуйста, курите вы или нет" << endl;
+
+    for (int i = 0; ;)
+    {
+        cout << "Вы курите? <yes/no>";
+
+        cin >> a;
+
+        if (a == "yes" && smoking_place < 5)
+        {
+            i++;
+            Ar[i] = i;
+            smoking_place = Ar[i];
+            cout << "Ваше место:" << smoking_place << endl;
+            int all_places = i + b;
+            if (all_places == 10)
+            {
+                cout << "Места закончились" << endl;
+                break;
+            }
+            continue;
+        }
+        if (a == "no" && no_smoking_place < 10)
+        {
+            b++;
+            Mass[b] = b + 5;
+            no_smoking_place = Mass[b];
+            cout << "Ваше место:" << no_smoking_place << endl;
+            int all_places = i + b;
+            if (all_places == 10)
+            {
+                cout << "Места закончились" << endl;
+                break;
+            }
+            continue;
+        }
+
+    }
+    return 0;
 }
 
 // Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
